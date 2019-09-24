df2a92c352bd8d389a7925a0ef7c0c2769e989bb
Filimon <filimon.danopoulos@stratsys.se>
2019-09-24 13:08:45 +0200

    Added security linting

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -20,6 +20,9 @@ module.exports = {
   plugins: [
     'prettier',
     '@typescript-eslint',
+    'scanjs-rules',
+    'no-unsanitized',
+    'no-wildcard-postmessage'
   ],
   rules: {
     'prettier/prettier': [
@@ -35,10 +38,40 @@ module.exports = {
       }
     ],
     '@typescript-eslint/no-use-before-define': 'off',
-    strict: 'off',
+    'strict': 'off',
     'no-undef': 'off',
     'no-unused-vars': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'scanjs-rules/accidental_assignment': 'error',
+    'scanjs-rules/assign_to_hostname' : 'error',
+    'scanjs-rules/assign_to_href' : 'error',
+    'scanjs-rules/assign_to_location' : 'error',
+    'scanjs-rules/assign_to_onmessage' : 'error',
+    'scanjs-rules/assign_to_pathname' : 'error',
+    'scanjs-rules/assign_to_protocol' : 'error',
+    'scanjs-rules/assign_to_search' : 'error',
+    'scanjs-rules/assign_to_src' : 'error',
+    'scanjs-rules/call_Function' : 'error',
+    'scanjs-rules/call_addEventListener_deviceproximity' : 'error',
+    'scanjs-rules/call_addEventListener_message' : 'error',
+    'scanjs-rules/call_connect' : 'error',
+    'scanjs-rules/call_eval' : 'error',
+    'scanjs-rules/call_execScript' : 'error',
+    'scanjs-rules/call_hide' : 'error',
+    'scanjs-rules/call_open_remote=true' : 'error',
+    'scanjs-rules/call_parseFromString' : 'error',
+    'scanjs-rules/call_setImmediate' : 'warning',
+    'scanjs-rules/call_setInterval' : 'warning',
+    'scanjs-rules/call_setTimeout' : 'warning',
+    'scanjs-rules/new_Function' : 'error',
+    'scanjs-rules/property_addIdleObserver' : 'error',
+    'scanjs-rules/property_createContextualFragment' : 'error',
+    'scanjs-rules/property_crypto': 'error',
+    'scanjs-rules/property_indexedDB' : 'error',
+    'scanjs-rules/property_mgmt' : 'error',
+    'no-unsanitized/method': 'error',
+    'no-unsanitized/property': 'error',
+    'no-wildcard-postmessage/no-wildcard-postmessage': 'error'
   }
 }
