1ca92c35381d0a0a409ff407f2d7dda364a86eb8
Filimon <filimon.danopoulos@stratsys.se>
2019-09-24 11:57:10 +0200

    Added secure coding practices to linting

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es6: true
+  },
+  plugins: [
+    'scanjs-rules',
+    'no-unsanitized',
+    'no-wildcard-postmessage'
+  ],
+  extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
   rules: {
     'prettier/prettier': [
       'error',
@@ -13,17 +25,51 @@ module.exports = {
       }
     ],
     '@typescript-eslint/no-use-before-define': 'off',
-    strict: 'off',
+    'strict': 'off',
     'no-undef': 'off',
     'no-unused-vars': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/no-v-html': 'error',
+    'scanjs-rules/accidental_assignment': 'error',
+    'scanjs-rules/assign_to_hostname' : 'error',
+    'scanjs-rules/assign_to_href' : 'error',
+    'scanjs-rules/assign_to_location' : 'error',
+    'scanjs-rules/assign_to_onmessage' : 'error',
+    'scanjs-rules/assign_to_pathname' : 'error',
+    'scanjs-rules/assign_to_protocol' : 'error',
+    'scanjs-rules/assign_to_search' : 'error',
+    'scanjs-rules/assign_to_src' : 'error',
+    'scanjs-rules/call_Function' : 'error',
+    'scanjs-rules/call_addEventListener' : 'error',
+    'scanjs-rules/call_addEventListener_deviceproximity' : 'error',
+    'scanjs-rules/call_addEventListener_message' : 'error',
+    'scanjs-rules/call_connect' : 'error',
+    'scanjs-rules/call_eval' : 'error',
+    'scanjs-rules/call_execScript' : 'error',
+    'scanjs-rules/call_hide' : 'error',
+    'scanjs-rules/call_open_remote=true' : 'error',
+    'scanjs-rules/call_parseFromString' : 'error',
+    'scanjs-rules/call_setImmediate' : 'error',
+    'scanjs-rules/call_setInterval' : 'error',
+    'scanjs-rules/call_setTimeout' : 'error',
+    'scanjs-rules/identifier_indexedDB' : 'error',
+    'scanjs-rules/identifier_localStorage' : 'error',
+    'scanjs-rules/identifier_sessionStorage' : 'error',
+    'scanjs-rules/new_Function' : 'error',
+    'scanjs-rules/property_addIdleObserver' : 'error',
+    'scanjs-rules/property_createContextualFragment' : 'error',
+    'scanjs-rules/property_crypto': 'error',
+    'scanjs-rules/property_geolocation' : 'error',
+    'scanjs-rules/property_getUserMedia' : 'error',
+    'scanjs-rules/property_indexedDB' : 'error',
+    'scanjs-rules/property_localStorage' : 'error',
+    'scanjs-rules/property_mgmt' : 'error',
+    'scanjs-rules/property_sessionStorage' : 'error',
+    'no-unsanitized/method': 'error',
+    'no-unsanitized/property': 'error',
+    'no-wildcard-postmessage/no-wildcard-postmessage': 'error'
   },
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
   parserOptions: {
     parser: '@typescript-eslint/parser'
   },
